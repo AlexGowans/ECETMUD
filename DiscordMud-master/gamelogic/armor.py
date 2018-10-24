d9be05d55963c8bbee9e969c18c58cb6b3e14494
AlexGowans <alexgowans@hotmail.co.uk>
2018-10-23 21:18:42 -0700

    Made some changes to armour, removed 'coverage' and went with straight armour value, added 2 new helmets, removed original helmet

--- DiscordMud-master/gamelogic/armor.py
+++ DiscordMud-master/gamelogic/armor.py
@@ -4,29 +4,34 @@ from random import random
 
 
 class Helmet(HeadArmor):
-    def __init__(self, coverage: float = 0., *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.Coverage = coverage  # Pct. of head the helmet covers. [0, 1]
 
     @property
     def ArmorCount(self):
         # Determine if the bullet hits or misses
-        return self._ArmorCount if random.random() <= self.Coverage else 0
+        return self._ArmorCount
 
     @ArmorCount.setter
     def ArmorCount(self, val):
         self._ArmorCount = val
 
 
-class SSh68(Helmet, FullyImplemented):
-    """
-    Based on the Soviet SSh-68 helmet
-    """
-    Name: str = "SSh-68"
+class clothHeadband(Helmet, FullyImplemented):
+
+    Name: str = "Cloth Headband"
 
     def __init__(self):
-        super().__init__(coverage=.50,
-                         armor_count=1,
+        super().__init__(armor_count=1,
+                         name=self.Name,
+                         weightlb=3.31)
+
+class leatherHelmet(Helmet, FullyImplemented):
+
+    Name: str = "Leather Helmet"
+
+    def __init__(self):
+        super().__init__(armor_count=1,
                          name=self.Name,
                          weightlb=3.31)
 
